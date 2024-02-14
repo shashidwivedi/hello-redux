cc3d05df11841a4578859ea8c66dcf541650b9a3
Shashi Dwivedi <srdwivedi16@gmail.com>
2024-02-14 22:30:47 +0530

    add redux and update component to get app state from store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
 import HelloWorld from "./HelloWorld";
+import reducer from "./reducers";
+import { createStore } from "redux";
 
-const App = () => {
-  const [tech] = useState("React");
-  return <HelloWorld tech={tech} />;
-};
+const initialState = { tech: "React " };
+const store = createStore(reducer, initialState);
+
+class App extends React.Component {
+  render() {
+    return <div>
+      <HelloWorld tech={store.getState().tech} />
+    </div>
+  }
+}
 
 export default App;
